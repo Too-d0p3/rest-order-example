fed459cabd8f59af4830ab51f1b3986b2275bc3e
Ondřej Nevřela <ondra@DESKTOP-RQSANHH.localdomain>
2025-06-06 00:38:06 +0200

    add SerializerInterface to OrderController

--- app/src/Presentation/Controller/OrderController.php
+++ app/src/Presentation/Controller/OrderController.php
@@ -3,9 +3,15 @@
 namespace App\Presentation\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/orders')]
 class OrderController extends AbstractApiController
 {
+    private SerializerInterface $serializer;
 
+    public function __construct(SerializerInterface $serializer)
+    {
+        $this->serializer = $serializer;
+    }
 }
